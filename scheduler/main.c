b022548ae6f14d94f83797be9d639102a2373700
Gregory Nietsky <gregory@distrotech.co.za>
2013-02-04 12:48:04 +0000

    Submitted By:            Armin K. <krejzi at email dot com> Date:                    2013-01-24 Initial Package Version: 1.6.1 Upstream Status:         Fixed upstream Origin:                  Upstream Description:             Move file, directory, user and group configuration to a separate                          file. Also warn about directives that have moved and set default                          cups-files.conf. Fixes CVE-2012-5591                          http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2012-5519
    git-svn-id: http://ns2/svnsrc/distro/cups/cups-1.6.1@4488 a03fbada-158d-47ef-9f12-77e997805b15

--- scheduler/main.c
+++ scheduler/main.c
@@ -207,7 +207,6 @@ main(int  argc,				/* I - Number of command-line args */
 
                 char *current;		/* Current directory */
 
-
 	       /*
 	        * Allocate a buffer for the current working directory to
 		* reduce run-time stack usage; this approximates the
@@ -233,6 +232,35 @@ main(int  argc,				/* I - Number of command-line args */
 		cupsdSetStringf(&ConfigurationFile, "%s/%s", current, argv[i]);
 		free(current);
               }
+
+	      if (!CupsFilesFile)
+	      {
+	        char	*filename,	/* Copy of cupsd.conf filename */
+			*slash;		/* Final slash in cupsd.conf filename */
+		size_t	len;		/* Size of buffer */
+
+		len = strlen(ConfigurationFile) + 15;
+		if ((filename = malloc(len)) == NULL)
+		{
+		  _cupsLangPrintf(stderr,
+		                  _("cupsd: Unable to get path to "
+		                    "cups-files.conf file."));
+                  return (1);
+		}
+
+		strlcpy(filename, ConfigurationFile, len);
+		if ((slash = strrchr(filename, '/')) == NULL)
+		{
+		  _cupsLangPrintf(stderr,
+		                  _("cupsd: Unable to get path to "
+		                    "cups-files.conf file."));
+                  return (1);
+		}
+
+		strlcpy(slash, "/cups-files.conf", len - (slash - filename));
+		cupsdSetString(&CupsFilesFile, filename);
+		free(filename);
+	      }
 	      break;
 
           case 'f' : /* Run in foreground... */
@@ -271,6 +299,29 @@ main(int  argc,				/* I - Number of command-line args */
 	      UseProfiles = 0;
 	      break;
 
+          case 's' : /* Set cups-files.conf location */
+              i ++;
+	      if (i >= argc)
+	      {
+	        _cupsLangPuts(stderr, _("cupsd: Expected cups-files.conf "
+	                                "filename after \"-s\" option."));
+	        usage(1);
+	      }
+
+              if (argv[i][0] != '/')
+	      {
+	       /*
+	        * Relative filename not allowed...
+		*/
+
+	        _cupsLangPuts(stderr, _("cupsd: Relative cups-files.conf "
+	                                "filename not allowed."));
+	        usage(1);
+              }
+
+	      cupsdSetString(&CupsFilesFile, argv[i]);
+	      break;
+
 #ifdef __APPLE__
           case 'S' : /* Disable system management functions */
               fputs("cupsd: -S (disable system management) for internal "
@@ -298,7 +349,10 @@ main(int  argc,				/* I - Number of command-line args */
     }
 
   if (!ConfigurationFile)
+  {
     cupsdSetString(&ConfigurationFile, CUPS_SERVERROOT "/cupsd.conf");
+    cupsdSetString(&CupsFilesFile, CUPS_SERVERROOT "/cups-files.conf");
+  }
 
  /*
   * If the user hasn't specified "-f", run in the background...
@@ -483,17 +537,11 @@ main(int  argc,				/* I - Number of command-line args */
   */
 
   if (!cupsdReadConfiguration())
-  {
-    if (TestConfigFile)
-      printf("%s contains errors\n", ConfigurationFile);
-    else
-      syslog(LOG_LPR, "Unable to read configuration file \'%s\' - exiting!",
-	     ConfigurationFile);
     return (1);
-  }
   else if (TestConfigFile)
   {
-    printf("%s is OK\n", ConfigurationFile);
+    printf("\"%s\" is OK.\n", CupsFilesFile);
+    printf("\"%s\" is OK.\n", ConfigurationFile);
     return (0);
   }
 
